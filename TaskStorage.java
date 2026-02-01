0112107049610e2c31229e5c5f46c18805604a0b
suvani-ctrl <suvanibasnet8@gmail.com>
2026-02-01 10:09:25 +0545

    Day 2 code cleanup

---
+++ TaskStorage.java
@@ -0,0 +1,17 @@
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.*;
+import java.io.IOException;
+
+public class TaskStorage {
+    
+    public static String main
+
+}
+
+
+
+
+
+
+    
--- Tasktracker.java
+++ Tasktracker.java
@@ -1,59 +1,82 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-
+import java.util.concurrent.atomic.AtomicLong;
+import com.fasterxml.jackson.databind.ObjectMapper;
 public class Tasktracker {
 
-    private static ArrayList<String> tasks = new ArrayList<>();
-
-    public static void updateTask(String[] args){
     
+    static class Task{
+        long id;
+        String taskDescription;
+        String username;
+        String displayName;
+        String createdAt;
+        String updatedAt;
 
-
+        public Task(
+            long id,
+            String taskDescription,
+            String username,
+            String displayName,
+            String createdAt,
+            String updatedAt
+        ){
+            this.id = id;
+            this.taskDescription = taskDescription;
+            this.username = username;
+            this.displayName = displayName;
+            this.createdAt = createdAt;
+            this.updatedAt = updatedAt;
+        }
     }
 
-    public static void deleteTask(String[] args){
 
-    }
-
-    public static void editTask(String[] args){
-
-    }
-    public static void listTask(){
-        if(tasks.isEmpty()){
+      public static String listTask(){
+        if(Tasks.isEmpty()){
             System.out.println("No tasks found");
-            return;
+            return "No tasks fond";
         }
-        System.out.println("Your tasks: ");
-        for(String task: tasks){
-            System.out.println(" . "+task);
+        try (FileWriter fw = new FileWriter("tasks.json")){
+            fw.write("[");
+            for (int i = 0 ; i<Tasks.size(); i++){
+                Task t = Tasks.get(i);
+                fw.write("{\"id\":" + t.id +
+                     ",\"description\":\"" + t.taskDescription + "\"" +
+                     ",\"username\":\"" + t.username + "\"}");
+                     if (i < Tasks.size() - 1) fw.write(",");
+            }
+            fw.write("]");
+            System.out.println("Tasks saved to json");
+            
+        } catch (Exception e) {
+                e.printStackTrace();
         }
+        return "Tasks are listed";
+    }
+        
+
+    public static String markTaskAsDone(){
+
+        return "Task Marked As Done";
     }
 
-    public static void markTaskAsDone(){
-    
 
-    }
-
-
-    private static String createuserProfile(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the username:");
-        String username = scanner.nextLine();
-        System.out.println("Enter the display name:");
+    private static String UserProfile(){
+       String username = scanner.nextLine();
+          System.out.println("Enter the username:");
+       System.out.println("Enter the display name:");
         String displayname = scanner.nextLine();
         System.out.println("Enter the task Description:");
         String taskDescription = scanner.nextLine();
         System.out.println("!!! Wait while we grant you a task Id !!!");
         String createdAt = LocalDateTime.now().toString();
         String updatedAt = createdAt;
-        scanner.close();
-        return "Profile created for" + username;
+        return "Profile created for" + username + "with display name of"+ displayname + "named"+ taskDescription + "created at"+createdAt+"updated at last:" + updatedAt;
     }
-
-
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
         while(true){
             System.out.println("Task Tracker");
             System.out.println("1. Create a Task");
@@ -62,31 +85,24 @@ public class Tasktracker {
             System.out.println("4.Status of Task");
             System.out.println("5. Mark task as done");
             System.out.println("Choose:");
-
             String choice = scanner.nextLine();
-
             switch(choice){
                 case "1":
                     String message = createuserProfile();
                     System.out.println(message);
                     break;
-
                 case "2":
                     listTask();
                     break;
-
                 case "3":
                     deleteTask();
                     break;
-
                 case "4":
                     listTask();
                     break;
-
                 case "5":
-                    TaskmarkDone();
+                    markTaskAsDone();
                     break;
-
                 case "6":
                     System.out.println("Bye");
                     System.exit(0);
@@ -96,6 +112,8 @@ public class Tasktracker {
                     break;
             }
         }
+        scanner.close();
     }
 
 }
+
